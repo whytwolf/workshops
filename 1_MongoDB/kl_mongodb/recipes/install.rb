197f9ba08dd465a323a3fbdea7bb27e6f5bf1cb1
Kevin Lewis <kevin@whytwolf.net>
2022-01-18 09:23:48 -0700

    Added comments to recipes.

--- 1_MongoDB/kl_mongodb/recipes/install.rb
+++ 1_MongoDB/kl_mongodb/recipes/install.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
 
+# Create the MongoDB repository.
 yum_repository 'mongodb' do
   description 'MongoDB Repository'
   baseurl 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/'
@@ -11,8 +12,10 @@ yum_repository 'mongodb' do
   enabled true
 end
 
+# Install the MongoDB packages.
 package 'mongodb-org'
 
+# Enable and start the mongod service.
 service 'mongod' do
   action [:enable, :start]
 end
