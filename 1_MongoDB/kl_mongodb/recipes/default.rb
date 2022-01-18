b4b51a8c6f658cb71516fc38e6a26d54866f14d7
Kevin Lewis <kevin@whytwolf.net>
2022-01-18 08:55:33 -0700

    Added selinux resources. Created cookbook file mongodb_cgroup_memory.te.

--- 1_MongoDB/kl_mongodb/recipes/default.rb
+++ 1_MongoDB/kl_mongodb/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+%w(selinux install).each do |recipe|
+  include_recipe kl_mongodb::recipe
+end
--- 1_MongoDB/kl_mongodb/recipes/selinux.rb
+++ 1_MongoDB/kl_mongodb/recipes/selinux.rb
@@ -3,3 +3,10 @@
 # Recipe:: selinux
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+selinux_install 'selinux_tools'
+
+selinux_module 'mongodb_cgroup_memory' do
+  source 'selinux/mongodb_cgroup_memory.te'
+  action :create
+end
