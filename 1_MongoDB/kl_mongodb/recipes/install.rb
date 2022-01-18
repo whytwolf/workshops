f23e26943f25418940d6d43725d9bb12964a5929
Kevin Lewis <kevin@whytwolf.net>
2022-01-18 08:59:14 -0700

    Added mongodb install resources.

--- 1_MongoDB/kl_mongodb/recipes/install.rb
+++ 1_MongoDB/kl_mongodb/recipes/install.rb
@@ -3,3 +3,16 @@
 # Recipe:: install
 #
 # Copyright:: 2022, The Authors, All Rights Reserved.
+
+yum_repository 'mongodb' do
+  description 'MongoDB Repository'
+  baseurl http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/
+  gpgcheck false
+  enabled true
+end
+
+package 'mongodb-org'
+
+service 'mongod'
+  action [:enable, :start]
+end
